6be76c08856b983054348090496b2a04924f59c2
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-02-25 22:04:41 +0100

    docstrings, renamed submodule, cleanup

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -1,35 +1,19 @@
 import pytest
-from ambio.distance import hammingDistance, editDistance, generateEditDistanceTable
+from ambio import algs
 
 
 def test_hamming_not_same_length():
     with pytest.raises(ValueError):
-        hammingDistance("abcd", "ef")
+        algs.hammingDistance("abcd", "ef")
 
 
 def test_hamming_same():
-    assert hammingDistance("abcd", "abcd") == 0
+    assert algs.hammingDistance("abcd", "abcd") == 0
 
 
 def test_hamming_close():
-    assert hammingDistance("abcd", "abef") == 2
+    assert algs.hammingDistance("abcd", "abef") == 2
 
 
 def test_hamming_different():
-    assert hammingDistance("abcd", "efgh") == 4
-
-
-def test_edit_empty():
-    assert editDistance("abcd", "") == 4
-
-
-def test_edit_same():
-    assert editDistance("abcd", "abcd") == 0
-
-
-def test_edit_close():
-    assert editDistance("abcd", "abe") == 2
-
-
-def test_edit_different():
-    assert editDistance("abcd", "efgh") == 4
+    assert algs.hammingDistance("abcd", "efgh") == 4
