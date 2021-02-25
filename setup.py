5dbee220329c35beb53c8fe31d2ea56df5cabec2
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-02-25 01:11:55 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ambio',
+      version='0.1',
+      description='A lightweight Bioinformatics library',
+      url='https://github.com/albertomosconi/ambio',
+      author='Alberto Mosconi',
+      license='MIT',
+      packages=['ambio'],
+      zip_safe=False)
