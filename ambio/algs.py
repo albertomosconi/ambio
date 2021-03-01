b67f1a2eb1f20dc33de1602d201450f91a5ef121
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2021-03-01 12:34:56 +0100

    expand documentation

--- ambio/algs.py
+++ ambio/algs.py
@@ -9,6 +9,13 @@ def hammingDistance(
 ):
     """Calculates the Hamming distance between two strings.
 
+    The Hamming distance between two strings of equal length is the number of positions
+    at which the corresponding symbols are different. In other words, it measures 
+    the minimum number of substitutions required to change one string into the other,
+    or the minimum number of errors that could have transformed one string into the other.
+
+    Learn more: `Hamming Distance [wikipedia] <https://en.wikipedia.org/wiki/Hamming_distance>`_
+
     :param str string1: The first string.
     :param str string2: The second string.
 
@@ -16,6 +23,16 @@ def hammingDistance(
 
     :return: The Hamming distance between `string1` and `string2`.
     :rtype: int
+
+    **Example code**
+
+    .. code:: python
+
+        >> hammingDistance("sunday", "sunray")
+        1
+
+        >> hammingDistance("hello", "world!")
+        ValueError: "The two given strings are not the same length."
     """
 
     if len(string1) != len(string2):
@@ -39,6 +56,8 @@ def alignmentScoreTable(
 ):
     """Generates the table of scores needed for finding the alignment score with the Needleman-Wunsch algorithm.
 
+    Learn more: `Needleman-Wunsch Algorithm [wikipedia] <https://en.wikipedia.org/wiki/Needleman-Wunsch_algorithm>`_
+
     :param str string1: The first string to align.
     :param str string2: The second string to align.
     :param bool paths: When True the table used for backtracking is also returned.
@@ -49,6 +68,26 @@ def alignmentScoreTable(
     :return: A matrix containing the alignment scores, and 
         optionally another matrix with the origin cells.
     :rtype: list
+
+    **Example code**
+
+    .. code:: python
+
+        >> scores, paths = alignmentScoreTable("home", "house", paths=True)
+        >> scores
+        [[ 0,  -2, -4, -6, -8],
+         [-2,   1, -1, -3, -5],
+         [-4,  -1,  2,  0, -2],
+         [-6,  -3,  0,  1, -1],
+         [-8,  -5, -2, -1,  0],
+         [-10, -7, -4, -3,  0]]
+        >> paths
+        [[(-1,-1), (0, 0), (0, 1), (0, 2), (0, 3)],
+         [ (0, 0), (0, 0), (1, 1), (1, 2), (1, 3)],
+         [ (1, 0), (1, 1), (1, 1), (2, 2), (2, 3)],
+         [ (2, 0), (2, 1), (2, 2), (2, 2), (3, 3)],
+         [ (3, 0), (3, 1), (3, 2), (3, 3), (3, 3)],
+         [ (4, 0), (4, 1), (4, 2), (4, 3), (4, 3)]]
     """
 
     tab = [[0 for i in range(len(string1) + 1)]
@@ -101,6 +140,11 @@ def alignmentScore(
 ):
     """Calculate the alignment score of two given strings using the Needleman–Wunsch algorithm.
 
+    The algorithm essentially divides a large problem (e.g. the full sequence) into a series of smaller problems, 
+    and it uses the solutions to the smaller problems to find an optimal solution to the larger problem.
+
+    Learn more: `Needleman-Wunsch Algorithm [wikipedia] <https://en.wikipedia.org/wiki/Needleman-Wunsch_algorithm>`_
+
     :param str string1: The first string to align.
     :param str string2: The second string to align.
     :param bool paths: When True the table used for backtracking is also returned.
@@ -111,6 +155,13 @@ def alignmentScore(
     :return: A matrix containing the alignment scores, and 
         optionally another matrix with the origin cells.
     :rtype: list
+
+    **Example code**
+
+    .. code:: python
+
+        >> alignmentScore("sunday", "sunray")
+        4
     """
 
     table = alignmentScoreTable(
@@ -128,6 +179,8 @@ def showAlignment(
 ):
     """Returns a visual representation of the alignment of two given strings using the Needleman–Wunsch algorithm.
 
+    Learn more: `Needleman-Wunsch Algorithm [wikipedia] <https://en.wikipedia.org/wiki/Needleman-Wunsch_algorithm>`_
+
     :param str string1: The first string to align.
     :param str string2: The second string to align.
     :param int insertionDeletionWeight: The cost of an insertion or deletion, default is -2.
@@ -137,6 +190,15 @@ def showAlignment(
     :return: A list containing the two strings modified to show 
         which edits have been made for them to be aligned.
     :rtype: list
+
+    **Example code**
+
+    .. code:: python
+
+        >> al = showAlignment("sunday", "sunray")
+        >> print(al[0] + "\\n" + al[1])
+        s--unday
+        saturday
     """
 
     _, track = alignmentScoreTable(
